740dcc4bcac3ef2b4fdf0a73babe251b13558bba
Pavel Giruts <pavel.giruts@talkthinkdo.com>
2025-02-05 12:48:01 +0300

    changed style to material

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopbarComponent } from '../topbar/topbar.component';
-import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-main',
-  imports: [RouterOutlet, TopbarComponent, MenuComponent],
+  imports: [RouterOutlet, TopbarComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
--- src/app/components/topbar/topbar.component.ts
+++ src/app/components/topbar/topbar.component.ts
@@ -1,8 +1,17 @@
 import { Component } from '@angular/core';
+import {MatToolbar} from '@angular/material/toolbar';
+import {MatAnchor} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-topbar',
-  imports: [],
+  imports: [
+    MatToolbar,
+    MatIcon,
+    MatAnchor,
+    RouterLink
+  ],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.css'
 })
