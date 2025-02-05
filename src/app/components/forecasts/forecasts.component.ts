8805c7f286f70ed0a7277e18730060a1d862c140
Pavel Giruts <pavel.giruts@talkthinkdo.com>
2025-02-05 12:05:16 +0300

    changed forecast table to material style

--- src/app/components/forecasts/forecasts.component.ts
+++ src/app/components/forecasts/forecasts.component.ts
@@ -1,13 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 import {ForecastViewModel} from '../../interfaces/forecast-view-model';
 import {ApiService} from '../../services/api/api.service';
-import {AsyncPipe, DatePipe, DecimalPipe} from '@angular/common';
+import {DatePipe, DecimalPipe} from '@angular/common';
 import {FormsModule} from '@angular/forms';
 import {Router} from '@angular/router';
 import {Observable} from 'rxjs';
 import {MatDialog} from '@angular/material/dialog';
 import {ConfirmDialogComponent} from '../confirm-dialog/confirm-dialog.component';
 import {IdShareService} from '../../services/id-share/id-share.service';
+import {MatButton} from '@angular/material/button';
+import {MatCell, MatCellDef, MatColumnDef, MatHeaderCell, MatHeaderCellDef, MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef, MatTable} from '@angular/material/table';
 
 @Component({
   selector: 'app-forecasts',
@@ -15,14 +17,24 @@ import {IdShareService} from '../../services/id-share/id-share.service';
     DatePipe,
     DecimalPipe,
     FormsModule,
-    AsyncPipe
+    MatButton,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatHeaderRow,
+    MatHeaderRowDef,
+    MatRow,
+    MatRowDef
   ],
   templateUrl: './forecasts.component.html',
   styleUrl: './forecasts.component.css'
 })
 export class ForecastsComponent implements OnInit {
 
-  headers: string[] = ['Date', '°C', '°F', 'Summary', 'Created', 'Modified'];
+  columns: string[] = ['date', 'temperatureC', 'temperatureF', 'summary', 'created', 'modified'];
   forecasts$: Observable<ForecastViewModel[]>;
   selectedForecastId: number | undefined;
 
